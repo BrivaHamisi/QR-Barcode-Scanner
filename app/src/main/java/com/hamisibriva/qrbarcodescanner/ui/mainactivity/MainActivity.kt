7beb1ef580e81b3c340f54b29fdf0da582ad4429
BrivaHamisi <brivahamisi@gmail.com>
2023-02-05 23:42:35 +0300

    Some Changes

--- app/src/main/java/com/hamisibriva/qrbarcodescanner/ui/mainactivity/MainActivity.kt
+++ app/src/main/java/com/hamisibriva/qrbarcodescanner/ui/mainactivity/MainActivity.kt
@@ -3,14 +3,28 @@ package com.hamisibriva.qrbarcodescanner.ui.mainactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hamisibriva.qrbarcodescanner.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Setting Up the ViewPager
         setViewPagerAdapter()
+
+        //Setting Up the bottom Navigation
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        bottomNavigationView.setOnClickListener(){
+
+        }
     }
 
     private fun setViewPagerAdapter() {
